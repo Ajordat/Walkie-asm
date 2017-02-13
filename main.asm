f1adcf324ff1ddb4960afed1f3ed4f139ffdfe85
ajordat <ls30687@salleurl.edu>
2017-02-13 12:30:25 +0100

    Codi inicial, s'ha de treballar a partir d'aquí.

--- main.asm
+++ main.asm
@@ -12,13 +12,18 @@
 ;* VARIABLES *
 ;*************
 
-	
+    SIZEH EQU 0x00
+    SIZEL EQU 0x01
 
 
 ;*************
 ;* CONSTANTS *
 ;*************
-
+    INIT_FSR0H EQU 0x00
+    INIT_FSR0L EQU 0x80
+    LOAD_CONST EQU 0xFF
+    SEND_BYTE EQU 0xFF
+    END_BYTE EQU 0xFF
 
 ;*********************************
 ; VECTORS DE RESET I INTERRUPCIÓ *
@@ -52,7 +57,60 @@ MAIN
 	call INIT_INTS
 	
 BUCLE
+	btfsc PIR1, RCIF, 0
+	goto RX
 	
+CHECK_LOAD_BTN
+    btfsc PORTC, 1, 0
+    goto LOAD_BTN
+    CLRF LOADED, 0
+    
+CHECK_SEND_BTN
+    btfsc PORTC, 0, 0
+    goto SEND_BTN
+    clrf SENT, 0
+    goto BUCLE	;AQUÍ FALTA EL TRACTAMENT DELS LEDS
+    
+RX
+    movff RCREG, WORD
+    movlw LOAD_CONST
+    cpfseq WORD, 0
+    goto SEND_MESSAGE
+    goto LOAD_MESSAGE
+    
+SEND_MESSAGE
+    
+LOAD_MESSAGE
+    movlw INIT_FSR0H
+    movwf FSR0H, 0
+    movlw INIT_FSR0L
+    movwf FSR0L, 0
+    
+    clrf SIZEH, 0
+    clrf SIZEL, 0
+    
+    movlw SEND_BYTE
+    movwf TXREG, 0
+    
+READ_MESSAGE
+    btfss PIR1, RCIF, 0
+    goto $-2
+    movff RCREG, WORD
+    
+    movlw END_BYTE
+    cpfseq WORD, 0
+    goto $+6
+    goto BUCLE
+    
+    incf SIZEL, 1, 0
+    btfsc STATUS, C, 0
+    incf SIZEH, 1, 0
+    
+    movff WORD, POSTINC0
+    movlw SEND_BYTE
+    movwf TXREG, 0
+    goto READ_MESSAGE
+    
 
 ;*******
 ;* END *
